dd55391c340c8799e2061407f8f6dc41920be0e2
Tatyana Polyanskaya <tn-79@mail.ru>
2018-01-27 17:50:06 +0300

    Добавлен production файл Add file for production

--- React/webpack.config.js
+++ React/webpack.config.js
@@ -1,10 +1,13 @@
-const path = require('path');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
-	devtool: 'source-map', 
+	devtool: NODE_ENV == 'development' ? 'source-map' : '',
 	entry: "./src/index",
 	output: {
 		filename: "build.js"
 	},
+	plugins: [],
 	module: {
 		loaders: [
 			{
@@ -20,5 +23,18 @@ module.exports = {
 				]
 			}
 		]
-	}
+	},
+
+}
+
+if (NODE_ENV != 'development') {
+	module.exports.plugins.push(	
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings:     false,
+				drop_console: true, 
+				unsafe:       true
+			}
+		})
+	);
 }
